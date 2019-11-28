f010a13d6338c29ba50b61cab203c8aa6cf76b71
styx11 <1654630248@qq.com>
2019-11-28 22:03:40 +0800

    :sparkles: update net_03.go for a echo server

---
+++ api/net/net_03.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+)
+
+func check(err error) {
+	if err != nil {
+		fmt.Println("Error at server:", err.Error())
+	}
+}
+
+// Writer or bufio.Writer will get stuck
+// so we use goruntine
+func handleEcho(conn net.Conn, ch chan []byte) {
+	for msg := range ch {
+		_, err := conn.Write(msg)
+		check(err)
+	}
+}
+
+// handle connect: receive message from client and start a goruntine to echo back
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+
+	msgChan := make(chan []byte)
+	go handleEcho(conn, msgChan)
+
+	fmt.Print("Echo:")
+	for {
+		buf := make([]byte, 512)
+		n, err := conn.Read(buf)
+		if err == io.EOF {
+			fmt.Println("")
+			close(msgChan)
+			break
+		}
+
+		fmt.Printf(" %s", buf[:n])
+		msgChan <- buf[:n]
+	}
+}
+
+// an Echo server
+func main() {
+	ln, err := net.Listen("tcp", ":8083")
+	check(err)
+	defer ln.Close()
+
+	fmt.Println("Server running at localhost:8083...")
+	for {
+		conn, err := ln.Accept()
+		check(err)
+
+		go handleConn(conn)
+	}
+}

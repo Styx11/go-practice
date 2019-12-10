e7f20f8dcb2f761beb0c51d122ca276c5298f266
styx11 <1654630248@qq.com>
2019-12-10 21:39:33 +0800

    :sparkles: update net_01 for practice 15.1

--- api/net/net_01.go
+++ api/net/net_01.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"io/ioutil"
 	"net"
+	"os"
+	"regexp"
 )
+
+var (
+	clients []string       //store client names
+	msgReg  *regexp.Regexp //match message from clients exclude client name and 'says: '
+	nameReg *regexp.Regexp //match clients
+)
+
+func init() {
+	msgReg = regexp.MustCompile(`says:\s([!-~\s]*)`) //all inputable characters
+	nameReg = regexp.MustCompile(`^([A-Z]+(?:\s[A-Z]+)?)\ssays\:\s`)
+}
 
 func check(err error) {
 	if err != nil {
@@ -12,24 +25,54 @@ func check(err error) {
 	}
 }
 
+// in ADMIN mode:
+// * The client can shut down the server by sending a command SH
+// * When the client sends the WHO command, the server displays a list of user names
 func handleConn(conn net.Conn) {
 	defer conn.Close()
 
-	fmt.Printf("Received from client:")
-	for {
-		buf := make([]byte, 512)
-		n, err := conn.Read(buf)
-		if err == io.EOF {
-			fmt.Println("")
-			break
-		}
-		check(err)
+	buf, err := ioutil.ReadAll(conn)
+	check(err)
+	rawMsg := string(buf)
+	clientMsg := msgReg.FindStringSubmatch(rawMsg)[1]
+	clientName := nameReg.FindStringSubmatch(rawMsg)[1]
 
-		fmt.Printf(" %s", buf[:n])
+	switch clientMsg {
+	case "SH":
+		if clientName != "ADMIN" {
+			fmt.Println("Only administrator has permission!")
+			return
+		}
+
+		fmt.Println("SH: We gonna shot it down")
+		conn.Close()
+		os.Exit(0)
+	case "WHO":
+		if clientName != "ADMIN" {
+			fmt.Println("Only administrator has permission!")
+			return
+		}
+
+		fmt.Println("WHO: This is the client list: 1:active, 0=inactive")
+		if len(clients) == 0 {
+			fmt.Println("No user yet")
+		}
+		for _, cli := range clients {
+			fmt.Printf("User %s is 1\n", cli)
+		}
+	default:
+		if clientName != "ADMIN" {
+			clients = append(clients, clientName)
+			fmt.Printf("Received data: %s\n", rawMsg)
+		} else {
+			fmt.Println("Unsupported admin command:", clientMsg)
+		}
 	}
 }
 
 // this example starts a tcp server receiving message from clients
+// rawMsg includes like: JOHN says: hello
+// more detail: https://github.com/unknwon/the-way-to-go_ZH_CN/blob/master/eBook/15.1.md
 func main() {
 	ln, err := net.Listen("tcp", ":8080")
 	check(err)
@@ -39,7 +82,6 @@ func main() {
 	for {
 		conn, err := ln.Accept()
 		check(err)
-
 		go handleConn(conn)
 	}
 }

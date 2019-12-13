949cb9540cef441617186cce7087f2a326678068
styx11 <1654630248@qq.com>
2019-12-13 21:07:05 +0800

    :sparkles: update net_02 for practice 15.1

--- api/net/net_02.go
+++ api/net/net_02.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"flag"
+	"bufio"
+	"bytes"
 	"fmt"
 	"net"
+	"os"
 	"strings"
 )
 
@@ -14,22 +16,37 @@ func check(err error) {
 }
 
 // net_02 creates a client to send a message from terminal to server localhost:8080
+// first: user name
+// then: message you want to send
+// more details: https://github.com/unknwon/the-way-to-go_ZH_CN/blob/master/eBook/15.1.md
 func main() {
-	flag.Parse()
-	args := flag.Args()
-	msg := strings.Join(args, " ")
-	if len(args) <= 0 {
-		fmt.Println("Please enter a message you want to send")
-		return
-	}
+	inputReader := bufio.NewReader(os.Stdout)
+	fmt.Print("Firstly, what's your name? (only upper name): ")
+	rawName, err := inputReader.ReadString('\n')
+	check(err)
+	clientName := strings.ToUpper(strings.Trim(rawName, "\n"))
 
 	// just dial to :8080
 	conn, err := net.Dial("tcp", "localhost:8080")
 	check(err)
 	defer conn.Close()
 
-	_, err = conn.Write([]byte(msg))
-	check(err)
+	var rawMsg []byte
+	for {
+		fmt.Print("Enter message (press Q to finish): ")
+		input, err := inputReader.ReadBytes('\n')
+		check(err)
 
-	fmt.Printf("Message have been sent: %s\n", msg)
+		input = bytes.Trim(input, "\n") //trim '\n'
+
+		if string(input) == "Q" {
+			clientMsg := clientName + " says: " + string(rawMsg)
+			_, err := conn.Write([]byte(clientMsg))
+			check(err)
+			fmt.Printf("Message has been sent: %s\n", clientMsg)
+			return
+		}
+
+		rawMsg = append(rawMsg, input...)
+	}
 }
